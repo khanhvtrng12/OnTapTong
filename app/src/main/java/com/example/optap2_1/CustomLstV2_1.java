2595dd8f3770b160abe4931995153d6cf69b0b89
khanhvtrng12 <khanhvtrng12@gmail.com>
2023-06-14 20:19:12 +0700

    UI 2.1 fixes
    Tạo Custom ListView

---
+++ app/src/main/java/com/example/optap2_1/CustomLstV2_1.java
@@ -0,0 +1,14 @@
+package com.example.optap2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomLstV2_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_lst_v21);
+    }
+}
