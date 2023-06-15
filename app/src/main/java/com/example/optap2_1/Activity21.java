5707161a8a6b1e4dce1a94f286341e6886310d19
vinhkhang123 <116474177+vinhkhang123@users.noreply.github.com>
2023-06-15 09:58:47 +0700

    Update Activity21.java

--- app/src/main/java/com/example/optap2_1/Activity21.java
+++ app/src/main/java/com/example/optap2_1/Activity21.java
@@ -10,11 +10,57 @@ import android.view.MenuItem;
 import android.widget.SearchView;
 
 public class Activity21 extends AppCompatActivity {
+    Activity21KhDatabase khoahocdatabase;
+    ListView lvkhoahoc;
+    ArrayList<Activity21KH> arrayList;
+    Activity21KhAdapter khoahocadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_21);
+         lvkhoahoc = (ListView) findViewById(R.id.lvKhoaHoc);
+        arrayList = new ArrayList<Activity21KH>();
+        khoahocadapter= new Activity21KhAdapter(this,R.layout.activity_21_list,arrayList);
+        lvkhoahoc.setAdapter(khoahocadapter);
+        //tạo Database
+        khoahocdatabase=new Activity21KhDatabase(this,"QuanLyKhoaHoc.sqlite",null,1);
+        //Xóa bảng
+        String sqlDrop="DROP TABLE KhoaHoc";
+        khoahocdatabase.QueryData(sqlDrop);
+        //Tạo bảng
+        String sql="CREATE TABLE IF NOT EXISTS KhoaHoc(Id INTEGER PRIMARY KEY AUTOINCREMENT,Ten nvarchar(80),HocPhi nvarchar(20))";
+        khoahocdatabase.QueryData(sql);
+
+        InitDataKhoaHoc();
+        LoadData();
     }
+     void InitDataKhoaHoc()
+    {
+        String sqlinsert1="insert into KhoaHoc values(1,'Lập Trình Android','5000000')";
+        String sqlinsert2="insert into KhoaHoc values(2,'Kỹ Thuật SEO Web','6500000')";
+        String sqlinsert3="insert into KhoaHoc values(3,'Google Adwords','4500000')";
+        khoahocdatabase.QueryData(sqlinsert1);
+        khoahocdatabase.QueryData(sqlinsert2);
+        khoahocdatabase.QueryData(sqlinsert3);
+    }
+    //load dữ liệu
+    public void LoadData()
+    {
+        String sqlSeclect="SELECT * FROM KhoaHoc";
+        Cursor dataKhoaHoc= khoahocdatabase.QueryGetData(sqlSeclect);
+
+        arrayList.clear();;
+        while (dataKhoaHoc.moveToNext())
+        {
+            int Id = dataKhoaHoc.getInt(0);
+            String strTen=dataKhoaHoc.getString(1);
+            String strHP=dataKhoaHoc.getString(2);
+            //  Toast.makeText(this,strTen,Toast.LENGTH_LONG).show();
+            arrayList.add(new Activity21KH(Id,strTen,strHP));
+        }
+        khoahocadapter.notifyDataSetChanged();
+    }
+
    @Override
    public boolean onCreateOptionsMenu(Menu m) {
        getMenuInflater().inflate(R.menu.menu_ot21, m);
@@ -22,11 +68,14 @@ public class Activity21 extends AppCompatActivity {
    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item1:
-               finish();
-                return true;
-            default:
+        int id = item.getItemId();
+        if (id == R.id.CauHinh)
+        {
+            Toast.makeText(this, "Bạn Đã Chọn Cấu hình", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.Thoat)
+        {
+            finish();
+        }
                 return super.onOptionsItemSelected(item);
         }
     }
